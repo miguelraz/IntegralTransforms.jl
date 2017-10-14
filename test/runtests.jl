e5b949f326960cbf47e5e3827d02677ca0b8fd33
Unknown <miguelraz@ciencias.unam.mx>
2017-10-14 00:53:02 -0500

    :fix: Docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 # Test setup
 
 #Beware of size of tests and time they take.
-
+tic()
 #@testsetup will run everything after the begin and end and report
 #Optional tic() toc() to observe large performance regressions.
 @testset "Abstract Hierarchies" begin
     include("testtypes.jl")
 end
+toc()
